f537699fd3130442e73df83964a14980766e4fbb
hironeko <hiroshikitahara@HiroshiKitahara-no-MacBook-Pro.local>
2017-03-29 17:47:34 +0900

    .emacs.d add && caks install && cask init done

---
+++ .emacs.d/init.el
@@ -0,0 +1,10 @@
+(require 'cask')
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(fset 'package-desc-vers 'package--ac-desc-version)
+(package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
